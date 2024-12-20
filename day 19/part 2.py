3a9ff4fcbccf5a942058161cacb18436291071c3
j-last <148649001+j-last@users.noreply.github.com>
2024-12-20 18:34:15 +0000

    day 19 part 2
    I take it back I had an idea using additional memoization that makes my day 19 part 2 compute much quicker

--- day 19/part 2.py
+++ day 19/part 2.py
@@ -1,24 +1,26 @@
 
 with open(r"day 19\towels.txt", "r") as f:
     towels = f.read().split(", ")
-    towels = ["r", "wr", "b", "g", "bwu", "rb", "gb", "br"]
 
 with open(r"day 19\designs.txt", "r") as f:
     designs = f.read().split("\n")
-    designs = """ubwu
-bwurrg""".split("\n")
 
 max_stripes = 0
 for towl in towels:
     max_stripes = max(max_stripes, len(towl))
 
 # -------------------------------------------------------------------
+
 doesnt_work = []
+works = {}
 def check_permutations_design(design, towels):
     total = 0
     def check_design(design, towels):
         nonlocal total
         total_at_start = total
+        if works.get(design) is not None:
+            total += works[design]
+            return
         if design == "":
             total += 1
             return
@@ -30,9 +32,11 @@ def check_permutations_design(design, towels):
                 check_design(design[i:], towels)
         if total == total_at_start:
             doesnt_work.append(design)
+        else:
+            works[design] = total - total_at_start
         return
+    
     check_design(design, towels)
-    print(total)
     return total
 
 
